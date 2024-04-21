531b082c90c2994284b0a77d2365e6fde5fedcf6
Nihal Shetty <nihalshetty2001@gmail.com>
2024-04-21 02:25:01 -0400

    Practicum 16

---
+++ resource-group.tf
@@ -0,0 +1,8 @@
+#
+# Creates a resource group in our azure account
+#
+
+resource "azurerm_resource_group" "practicum16" {
+    name = "practicum16"
+    location = "eastus"
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+# Sets global variables for this Terraform project.
+
+
+variable app_name {
+    default = "practicum16"
+}
+
+variable location {
+    default = "eastus"
+}
+
+variable kubernetes_version {
+}
